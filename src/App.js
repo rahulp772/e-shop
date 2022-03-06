c15b1aeba26522d5bab167230d58b74204691bb6
Rahul Prajapati <rahulprajapati@moneyphi.com>
2022-03-06 17:35:36 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { Routes, Route, useNavigate, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  let navigation = useNavigate();
+  console.log(navigation);
+  return <div>Hello from hatspage
+    <Link to="/">My Profile</Link>
+  </div>
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route exact path='/hatspage' element={<HatsPage />} />
+        <Route exact path='*' element={<div>No Route Found</div>} />
+      </Routes>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-
+import { useLocation } from "react-router-dom";
 import './homepage.styles.scss';
 
 import Directory from "../../components/directory/directory.component";
 
-const Homepage = () => (
-    <div className="homepage">
+const Homepage = () => {
+    let location = useLocation();
+    console.log(location);
+    return <div className="homepage">
         <Directory />
     </div>
-)
+}
 
 export default Homepage;
