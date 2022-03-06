f495e3330e4ccc39d5d5d4b9d81d507eb2a21e3e
Rahul Prajapati <rahulprajapati@moneyphi.com>
2022-03-06 20:51:15 +0530

    signin page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/header/header-component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Homepage />} />
         <Route exact path='/shop' element={<ShopPage />} />
+        <Route exact path='/signin' element={<SignInAndSignUpPage />} />
         <Route exact path='*' element={<div>No Route Found</div>} />
       </Routes>
     </div>
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({email: "", password: ""});
+    }
+
+    handleChange = (event) => {
+        const {value, name} = event.target;
+        this.setState({[name]: value});
+    }
+
+    render() {
+        return (
+            <div className="sign-in" autoComplete="off">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        autoComplete="off" 
+                        name="email" 
+                        type="email" 
+                        label='email' 
+                        value={this.state.email} 
+                        required 
+                    />
+                    <FormInput 
+                        autoComplete="off" 
+                        name="password" 
+                        type="password" 
+                        label='password' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        required 
+                    />
+                    <CustomButton type="submit" value="Submit Form">SIGN IN</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
