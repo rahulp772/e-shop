ea2bbe637fde8a6f25d2906ad62835c64dd07843
Rahul Prajapati <rahulprajapati@moneyphi.com>
2022-03-06 18:44:28 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React from 'react';
 import { Routes, Route, useNavigate, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => {
-  let navigation = useNavigate();
-  console.log(navigation);
-  return <div>Hello from hatspage
-    <Link to="/">My Profile</Link>
-  </div>
-}
+import ShopPage from './pages/shoppage/shop.component';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path='/' element={<Homepage />} />
-        <Route exact path='/hatspage' element={<HatsPage />} />
+        <Route exact path='/shop' element={<ShopPage />} />
         <Route exact path='*' element={<div>No Route Found</div>} />
       </Routes>
     </div>
