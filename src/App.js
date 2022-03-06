6ac89310c3e9789b28e4c91d2c3b6ef8709649c4
Rahul Prajapati <rahulprajapati@moneyphi.com>
2022-03-06 18:59:44 +0530

    added header page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Routes, Route, useNavigate, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header-component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route exact path='/' element={<Homepage />} />
         <Route exact path='/shop' element={<ShopPage />} />
