e1508a2050b801c70cf410a8f6fa947f9aefdad8
Rahul Prajapati <rahulprajapati@moneyphi.com>
2022-03-08 21:38:35 +0530

    solved infinite loop

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { connect, useSelector, useDispatch } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { connect, useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/header/header-component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, addUser, getUser, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from '../src/redux/user/user.action';
 
 function App(props) {
 
   const { currentUser } = useSelector((state) => state.user)
   console.log("CurrentUserState: ", currentUser);
+  console.log("PROPS", props);
 
   useEffect(() => {
     const { setCurrentUser } = props;
     auth.onAuthStateChanged(async (userAuth) => {
-      console.log("==========>", userAuth);
+      let user = null;
       if(userAuth) {
-        let userData = await createUserProfileDocument(userAuth);
-        setCurrentUser(userData)
-      } else {
-        setCurrentUser(null);
+        user = await createUserProfileDocument(userAuth);
       }
+      setCurrentUser(user);
     });
-  }, [props]);
-
+  }, []);
+  
     return (
       <div>
         <Header />
         <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route exact path='/shop' element={<ShopPage />} />
-          <Route exact path='/signin' element={<SignInAndSignUpPage />} />
+          {/* <Route exact path='/signin' element={<SignInAndSignUpPage />} /> */}
+          <Route exact path='/signin' element={ props.currentUser ? <Navigate to='/' /> : (<SignInAndSignUpPage />)} />
           <Route exact path='*' element={<div>No Route Found</div>} />
         </Routes>
       </div>
     );
-  }
+}
+
+const mapStateToProps = (({user}) => {
+  return { currentUser: user.currentUser }
+});
 
 const mapDispatchToProps = (dispatch) => {
   return { 
@@ -46,4 +50,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
